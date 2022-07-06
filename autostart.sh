adb5e45a796bfd8ca987ca5026328eb03b91e12c
Jellu <105118442+Debilesque@users.noreply.github.com>
2022-07-06 16:51:15 -0500

    Comments and improvements

--- autostart.sh
+++ autostart.sh
@@ -1,30 +1,64 @@
 #!/bin/sh
 
+############################################################
+
+# Script Name     :   Autostart
+# Description     :   WM-agnostic script for setting up
+#                     the environment
+# Args            :   none
+# Author          :   Jellu Cat
+
+############################################################
+
+#############
+# Functions #
+#############
+
+# Runs a program only if it isn't running
 run (){
     local name=$1
 
-    if pidof "$name"; then
+    if (pidof "$name" > /dev/null); then
         echo "$name already running!"
     else
-        if [ "$name" != "sxhkd" ]; then
-            echo "Running $name"
-            `$name` &
-        else
-            sxhkd -c ~/.config/sxhkd/sxhkdrc &
-            sxhkd -c ~/.config/sxhkd/bsphkd &
-        fi
+        echo "Running $name"
+       `$name` &
+    fi
+}
+
+# Runs a program killing any other instances, useful to restart after
+# changing the configuration files
+initialize (){
+    local name=$1
+
+    if (pidof "$name" > /dev/null); then
+        killall "$name"
+        echo "$name killed"
+    fi
+
+    if [ "$name" = "sxhkd" ]; then
+        sxhkd -c ~/.config/sxhkd/sxhkdrc &
+        sxhkd -c ~/.config/sxhkd/bsphkd &
+        echo "Running $name"
+    else
+       `$name` &
+        echo "Running $name"
     fi
 }
 
 setxkbmap latam &
 
 nitrogen --restore &
-run compton
 
+# Running some essential programs
+run compton
 run redshift
-run sxhkd
+initialize sxhkd
 run copyq
 
+# Somehow, if any change to xmodmap is to be done, the commands needs
+# to be one of the last
 xmodmap ~/.Xmodmap &
 
+# The status bar is the last thing loaded
 bash ~/.config/polybar/forest/launch.sh
